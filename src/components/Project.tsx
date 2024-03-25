b9215c125d754b75a107ce5453d700c25e14ee27
Hazghard <100840855+Hazghard@users.noreply.github.com>
2024-03-25 17:01:53 +0100

    Update Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,7 +12,7 @@ const Project = ({props}: Props) => {
   console.log("Project props",props);
   return (
     <>
-      <Link href="/projects/123" className='relative h-[200px] rounded-lg group cursor-pointer hover:scale-105 duration-300 border-2'>
+      <Link href={props.url} className='relative h-[200px] rounded-lg group cursor-pointer hover:scale-105 duration-300 border-2'>
           <Image src={props.image} alt='' height={150} width={200} className='rounded-lg w-full h-full'/>
           <div className='h-full w-full absolute top-0 left-0 flex justify-center items-center font-bold text-[#363a38] p-4 text-center rounded-lg opacity-0 group-hover:opacity-100 transition-all ease-in duration-300 bg-[#e3e9e7]'>
             {props.description}
