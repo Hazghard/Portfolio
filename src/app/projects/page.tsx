d5bdec2cff1cedb1e0c924ec5229d57ca03b3685
Hazghard <100840855+Hazghard@users.noreply.github.com>
2024-03-25 04:21:56 +0100

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,20 +6,19 @@ import React from 'react'
 import { getFiles, getProfile, getProjects, getSkills, getSocials } from "@/utils/sanity-utils";
 import {ProjectsType} from '@/app/types/ProjectsType'
 
-type ProjectsPageProps  = {
-  props:ProjectsType[]
-}
+type Props  = {}
 
-const ProjectsPage = async ({props}: ProjectsPageProps ) => {
+const ProjectsPage = async (props: Props ) => {
   const getSocialData = await getSocials();
   const getProfileFiles = await getFiles();
+  const projectData:ProjectsType[]=await getProjects()
   return (
     <section className='w-full bg-[#F1F6F9]'>
       <Navbar getSocialData={getSocialData} getProfileFiles={getProfileFiles} />
         <main className='max-w-7x1 min-h-screen mx-auto p-5'>
           <Motion delay={0.1} direction={''}>
             <div className='w-full my-4 md:my-16 p-5 grid grid-cols-1 md:grid-cols-3 gap-12'>
-            {props.map((s:ProjectsType,i:number)=>(<Project key={i} props={s} />))}
+              {projectData.map((s:ProjectsType,i:number)=>(<Project key={i} props={s} />))}
             </div>
           </Motion>
         </main>
