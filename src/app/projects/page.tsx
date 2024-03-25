16d76527b56887cd9599c403d1a283c3b3e2aa65
Hazghard <100840855+Hazghard@users.noreply.github.com>
2024-03-25 03:39:49 +0100

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,25 +3,27 @@ import Motion from '@/components/Motion'
 import Navbar from '@/components/Navbar'
 import Project from '@/components/Project'
 import React from 'react'
+import { getFiles, getProfile, getProjects, getSkills, getSocials } from "@/utils/sanity-utils";
+import {ProjectsType} from '@/app/types/ProjectsType'
 
-type Props = {}
+type Props = {
+  props:ProjectsType[]
+}
 
-const ProjectsPage = (props: Props) => {
+const ProjectsPage = async ({props}: Props) => {
+  const getSocialData = await getSocials();
+  const getProfileFiles = await getFiles();
   return (
     <section className='w-full bg-[#F1F6F9]'>
-        <Navbar/>
+      <Navbar getSocialData={getSocialData} getProfileFiles={getProfileFiles} />
         <main className='max-w-7x1 min-h-screen mx-auto p-5'>
           <Motion delay={0.1} direction={''}>
             <div className='w-full my-4 md:my-16 p-5 grid grid-cols-1 md:grid-cols-3 gap-12'>
-                <Project/>
-                <Project/>
-                <Project/>
-                <Project/>
-                <Project/>
+            {props.map((s:ProjectsType,i:number)=>(<Project key={i} props={s} />))}
             </div>
           </Motion>
         </main>
-        <Footer/>
+        <Footer props={getSocialData}/>
     </section>
   )
 }
